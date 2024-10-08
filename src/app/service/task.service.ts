9aa8f19586b91df43a6b7ae16198b99ba74b5a72
VuongMinh98 <155599700+VuongMinh12@users.noreply.github.com>
2024-10-08 16:06:50 +0700

    Update task.service.ts

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,7 +1,7 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ServiceInvoker } from './serviceInvocer';
+import { ServiceInvoker } from './serviceInvoker';
 
 
 @Injectable({
